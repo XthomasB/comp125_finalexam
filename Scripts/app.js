7cbd8e48fe30b282179726db4d6f23324bc72e90
XthomasB <jt.xbowen@gmail.com>
2016-08-17 14:16:41 -0400

    buttons displayed and dice images (blank)

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,6 +3,7 @@ var core;
     "use strict";
     var canvas;
     var stage;
+    
     // app entry function
     function init() {
         canvas = document.getElementById("canvas");
@@ -13,10 +14,31 @@ var core;
         main();
     }
     function gameLoop() {
-        stage.update();
+        stage.update(
+            
+        );
     }
     function main() {
+        var rollbutton = new createjs.Bitmap("Assets/images/rollButton.png"); 
+        
+        stage.addChild(rollbutton);
+        rollbutton.x = 250;
+        rollbutton.y = 250;   
+        var die1 = new createjs.Bitmap("Assets/images/blank.png");
+        stage.addChild(die1)
+        var die2 = new createjs.Bitmap("Assets/images/blank.png");   
+        stage.addChild(die2)
+        die2.x =400 
+            function rollbutton1(rollbutton) {
+                this.rollbutton = rollbutton;
+                die1 = (int)(Math.random()*6) + 1;
+                die2 = (int)(Math.random()*6) + 1;
+                }
+                
+                
+        
     }
+
     window.addEventListener("load", init);
 })(core || (core = {}));
 //# sourceMappingURL=app.js.map
